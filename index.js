57678a8e45adc91304bde5f9d910961061e5c10c
Samuel Asiabaka <67641796+samuellucier@users.noreply.github.com>
2022-07-13 09:35:25 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+let newCount = document.getElementById('count-el')
 
-let newCount = document.getElementById("count-el");
+let saveEl = document.getElementById('save-el')
 
-let saveEl = document.getElementById("save-el");
-
-let count = 0;
+let count = 0
 
 function increment() {
-    count += 1;
-    newCount.textContent = count;
-    console.log(count);
+  count += 1
+  newCount.textContent = count
+  console.log(count)
 }
 
 function save() {
-    let newSaveCount = ` ${count} -`;
-    saveEl.textContent += newSaveCount;
-    newCount.textContent = 0;
-    count = 0;
+  let newSaveCount = ` ${count} -`
+  saveEl.textContent += newSaveCount
+  newCount.textContent = 0
+  count = 0
 }
 
+let day = document.getElementById('date')
+day.textContent = new Date().toLocaleDateString('en-us', {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+  year: 'numeric',
+})
+
+function startTime() {
+  const date = new Date()
+  document.getElementById('timer').innerHTML = date.toLocaleTimeString()
+  setTimeout(function () {
+    startTime()
+  }, 1000)
+}
+
+startTime()
